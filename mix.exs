de0de26da546cbbf439f948e60d470357b988de4
Ron Tripp <ron.tripp@ahead.com>
2026-06-11 10:09:21 -0500

    Update to v.0.1.11

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Cyclium.MixProject do
   def project do
     [
       app: :cyclium,
-      version: "0.1.10",
+      version: "0.1.11",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
